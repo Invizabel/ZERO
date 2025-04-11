bf2a08d1aeb74c0b75c3e7a2ce32339ca4013a49
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-04-11 10:26:20 -0500

    Add files via upload

--- Chaff.js
+++ Chaff.js
@@ -1,10 +1,12 @@
+let math = require("math");
 let storage = require("storage");
 let free = storage.fsInfo("/ext")["freeSpace"];
 print("CREATING CHAFF");
+let data = ""; for (let i = 0; i < math.pow(2,8); i++) data += "0";
 let file = storage.openFile("/ext/CHAFF", "w", "create_always");
-for (let i = 0; i < free; i += 1)
+for (let i = 0; i < math.floor(free / math.pow(2,8)); i += 1)
 {
-    file.write("0");
+    file.write(data);
 }
 file.close();
 print("REMOVING CHAFF");
