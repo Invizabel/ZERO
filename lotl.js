26166e6af89f66edafeff70dd0075af008c38154
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-04-12 12:50:49 -0500

    Add files via upload

--- lotl.js
+++ lotl.js
@@ -1,4 +1,27 @@
-let math = require("math");
+function all(data)
+{
+    for (let i = 0; i < data.length; i++)
+    {
+        if (!data[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function any(data)
+{
+    for (let i = 0; i < data.length; i++)
+    {
+        if (data[i])
+        {
+            return true
+        }
+    }
+    return false;
+}
+
 function mean(data)
 {
     let hits = 0;
@@ -8,6 +31,10 @@ function mean(data)
     }
     return hits / data.length;
 }
+
+print(any([false,false,true]));
+print(any([false,false,false]));
+print(all([false,false,true]));
+print(all([false,false,false]));
+print(all([true,true,true]));
 print(mean([1,2,3]));
-print(mean([1,2,3,5]));
-print(mean([3,1,4,1,5]));
