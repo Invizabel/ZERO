b48a57c6a243ca83dfcf847efaf4d127bc1f3102
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-04-12 15:03:24 -0500

    Add files via upload

--- lotl.js
+++ lotl.js
@@ -1,3 +1,5 @@
+let math = require("math");
+
 function all(data)
 {
     for (let i = 0; i < data.length; i++)
@@ -60,9 +62,27 @@ function mean(data)
     return hits / data.length;
 }
 
+function varint(data)
+{
+    let SEGMENT_BITS = 0x7F;
+    let CONTINUE_BIT = 0x80;
+
+    while (true)
+    {
+        if ((data & ~SEGMENT_BITS) === 0) {;
+            return new_data.toString(16) + data.toString(16);
+        }
+
+        let new_data = ((data & SEGMENT_BITS) | CONTINUE_BIT);
+        data = math.floor(data / math.pow(2,7));
+    }
+}
+
 // Examples
 print(any(flatten_array([[[true],[false],[false]],[[false],[false],[false]]]))); // true
 print(any(flatten_array([[[false],[false],[false]],[[false],[false],[false]]]))); // false
 print(all(flatten_array([[[true],[true],[true]],[[true],[true],[true]]]))); // true
 print(all(flatten_array([[[true],[true],[true]],[[false],[false],[false]]]))); // false
 print(mean(flatten_array([[[1],[2],[3]],[[4],[5],[6]]]))); // 3.5
+print(varint(735)); // "df5"
+print(varint(770)); // "825"
