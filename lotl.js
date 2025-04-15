a471b9a015e9ee8d0c8837af06ce3cb752b4c4dc
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-04-15 09:22:26 -0500

    Add files via upload

--- lotl.js
+++ lotl.js
@@ -52,6 +52,11 @@ function flatten_array(data)
     }   
 }
 
+function hex(data)
+{
+    return ("0x" + data.toString(16)).slice(-2);;
+}
+
 function mean(data)
 {
     return sum(data) / data.length;
@@ -93,5 +98,6 @@ function sum(data)
 // Examples
 print(any(flatten_array([[[false],[false],[false]],[[false],[true],[false]]]))); // true
 print(all(flatten_array([[[true],[true],[true]],[[true],[true],[false]]]))); // false
+print(hex(770)); // 0x302
 print(mean(flatten_array([[[1],[2],[3]],[[4],[5],[6]]]))); // 3.5
-print("{" + split("bb{1:2}","{")[1]); // {1:2}
+print("{" + split("bb{1:2}{{aca","{")[1]); // {1:2}
